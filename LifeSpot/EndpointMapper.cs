94c89768e2687cb1b59e086c07bc3771d90b3148
Eugene Evseenko <jonikevseenko@gmail.com>
2022-09-07 22:26:23 +0600

    HW-03

---
+++ LifeSpot/EndpointMapper.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSpot
+{
+    public static class EndpointMapper
+    {
+        public static void MapCss(this IEndpointRouteBuilder builder)
+        {
+            var cssFiles = new[] { "index.css" };
+
+            foreach (var fileName in cssFiles)
+            {
+                builder.MapGet($"/Static/CSS/{fileName}", async context =>
+                {
+                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", fileName);
+                    var css = await File.ReadAllTextAsync(cssPath);
+                    await context.Response.WriteAsync(css);
+                });
+            }
+        }
+
+        public static void MapJavaScript(this IEndpointRouteBuilder builder)
+        {
+            var jsFiles = new[] { "main.js", "about.js", "testing.js" };
+
+            foreach (var fileName in jsFiles)
+            {
+                builder.MapGet($"/Static/JS/{fileName}", async context =>
+                {
+                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", fileName);
+                    var js = await File.ReadAllTextAsync(jsPath);
+                    await context.Response.WriteAsync(js);
+                });
+            }
+        }
+
+        public static void MapHtml(this IEndpointRouteBuilder builder)
+        {
+            var footer = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
+            var sidebar = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sidebar.html"));
+
+            builder.MapGet("/", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
+                var viewText = await File.ReadAllTextAsync(viewPath);
+
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sidebar)
+                    .Replace("<!--FOOTER-->", footer);
+
+                await context.Response.WriteAsync(html.ToString());
+            });
+
+            builder.MapGet("/testing", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
+
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sidebar)
+                    .Replace("<!--FOOTER-->", footer);
+
+                await context.Response.WriteAsync(html.ToString());
+            });
+
+            builder.MapGet("/about", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sidebar)
+                    .Replace("<!--FOOTER-->", footer);
+
+                await context.Response.WriteAsync(html.ToString());
+            });
+        }
+    }
+}
---
+++ LifeSpot/Static/JS/main.js
@@ -0,0 +1,50 @@
+﻿window.sessionStorage = {};
+handleSession();
+
+function logSession() {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Данные клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
+}
+
+function checkUser (isFirstVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        if (isFirstVisit)
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    } else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+};
+
+function handleSession() {
+    if (window.sessionStorage.getItem("startDate") == null) 
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    
+    if (window.sessionStorage.getItem("userAgent") == null) 
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст");
+        sessionStorage.setItem("userAge", input)
+        checkUser(true);
+    } else {
+        checkUser(false);
+    }
+    logSession();
+}
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+    var text = document.getElementById('searchInput').value.toLowerCase();
+    for (var i = 0; i < elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText.toLowerCase();
+        elements[i].style.display = videoText.includes(text)
+            ? 'inline-block'
+            : 'none';
+    }
+}
+
+function clearSession(){
+    window.sessionStorage.clear();
+}
